66844e336867cc14300d0daeed41556d7bb1a467
test <jadin.heaston@como.gov>
2024-01-02 22:24:02 -0600

    Removing unecessary TS.
    Signed-off-by: test <jadin.heaston@como.gov>

--- js/scripts.ts
+++ js/scripts.ts
@@ -1,36 +1,9 @@
 var timerIntervalHandle: number;
 
 document.addEventListener("DOMContentLoaded", () => {
-	initSelect2Inputs();
 	// Update the current time every second
 	timerIntervalHandle = setInterval(updateCurrentTime, 1000);
-
-	// //Confetti!!!!
-	// document.querySelectorAll("canvas.confetti").forEach((connfettiCanvas) => {
-	// 	if (!(connfettiCanvas instanceof HTMLCanvasElement))
-	// 		return false;
-	// 	showConfetti(connfettiCanvas);
-	// });
-
 });
-
-document.addEventListener('htmx:afterRequest', function (evt) {
-	initSelect2Inputs();
-});
-
-//Initializes all select2 question inputs.
-async function initSelect2Inputs() {
-	var select2Inputs = document.querySelectorAll('select.select2');
-	select2Inputs.forEach((element) => {
-		jQuery(element).select2();
-
-		//Manually focusing the search field when opened.
-		jQuery(element).on('select2:open', () => {
-			let select = document.querySelector('.select2-search__field') as HTMLSelectElement;
-			select.focus();
-		});
-	});
-}
 
 // TypeScript function to update current time
 function updateCurrentTime() {
@@ -67,7 +40,7 @@ function updateCurrentTime() {
 			}
 		};
 		const ordinalSuffix = nthNumber(now.getDate());
-		
+
 		// Combine the formatted time and date
 		const formattedDateTime = `${formattedTime} - ${formattedDate}${ordinalSuffix}`;
 
